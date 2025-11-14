45eadc92ec081abab92157cc791439048d44ddf3
Justin Schembri <j.schembri@tudelft.nl>
2025-11-14 16:24:52 +0100

    add a config

---
+++ src/config.py
@@ -0,0 +1,15 @@
+"""
+General configurations, variables and constant needed throughout the project.
+
+Run script in isolation to receive some basic information about the settings.
+"""
+#stdlib
+from pathlib import Path
+
+# Path assignments:
+ROOT = Path(__file__).parent.parent
+TRAINING_DATA_PATH = ROOT / "smsspamcollection"
+
+if __name__ == "__main__":
+    print(f"{ROOT=}")
+    print(f"{TRAINING_DATA_PATH=}")
