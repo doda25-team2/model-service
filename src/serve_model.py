f1ab8c013d2aeada42cca8a4147c4ac0a46a0957
Preethika <pajaykumar@tudelft.nl>
2025-11-18 14:23:51 +0100

    pull model from github release

--- src/serve_model.py
+++ src/serve_model.py
@@ -10,9 +10,47 @@ import pandas as pd
 #internal
 from config import OUTPUT_PATH
 from text_preprocessing import prepare, _extract_message_len, _text_process
+import os
+import requests
+from pathlib import Path
+import joblib
 
 app = Flask(__name__)
 swagger = Swagger(app)
+
+MODEL_PATH = Path(os.getenv("MODEL_PATH", "/models/model.joblib"))
+PREPROCESSOR_PATH = Path(os.getenv("PREPROCESSOR_PATH", "/models/preprocessor.joblib"))
+
+DEFAULT_MODEL_URL = os.getenv(
+    "DEFAULT_MODEL_URL",
+    "https://github.com/doda25-team2/model-service/releases/latest/download/model.joblib"
+)
+
+DEFAULT_PREPROCESSOR_URL = os.getenv(
+    "DEFAULT_PREPROCESSOR_URL",
+    "https://github.com/doda25-team2/model-service/releases/latest/download/preprocessor.joblib"
+)
+
+def download_if_missing(local_path: Path, url: str):
+    if local_path.exists():
+        return
+    print(f"{local_path} missing → downloading from {url}")
+
+    local_path.parent.mkdir(parents=True, exist_ok=True)
+    r = requests.get(url)
+    r.raise_for_status()
+    local_path.write_bytes(r.content)
+
+    print(f"Downloaded: {local_path}")
+
+
+def ensure_model_available():
+    download_if_missing(MODEL_PATH, DEFAULT_MODEL_URL)
+    download_if_missing(PREPROCESSOR_PATH, DEFAULT_PREPROCESSOR_URL)
+
+ensure_model_available()
+model = joblib.load(MODEL_PATH)
+preprocessor = joblib.load(PREPROCESSOR_PATH)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -39,8 +77,7 @@ def predict():
     """
     input_data = request.get_json()
     sms = input_data.get('sms')
-    processed_sms = prepare(sms)
-    model = joblib.load(OUTPUT_PATH / 'model.joblib')
+    processed_sms = preprocessor.transform([sms])
     prediction = model.predict(processed_sms)[0]
     
     res = {
